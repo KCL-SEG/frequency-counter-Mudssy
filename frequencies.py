cf86f2f5d280787b5a46568105bbc640be03bb74
Mudassir Ahmad <smahmad2002@gmail.com>
2022-12-09 10:29:05 +0000

    complete

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,11 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for item in items:
+        item =  str(item)
+        if item in frequencies.keys():
+            frequencies[item] +=1
+        else:
+            print(f'{item}')
+            frequencies.update({item: 1})
     return frequencies
